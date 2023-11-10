3143688ede3364910c0e0ccd9ddcfb68beda67a0
Muhammad Riyan Satrio Wibowo <150320075+r-ediewitsch@users.noreply.github.com>
2023-11-10 19:11:25 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const getElement = (identifier = "") => document.querySelector(identifier);
+
+function verifySubmission() {
+  let subjectElement = getElement("#subject").value;
+  let messageElement = getElement("#message").value;
+  let nameElement = getElement("#name").value;
+
+  if (subjectElement && messageElement && nameElement) {
+    let body = encodeURIComponent("From: " + nameElement + "\n" + messageElement);
+    window.location.href = "mailto:mriyansatriow@gmail.com?subject=" + subjectElement + "&body=" + body;
+  } 
+  else {
+    alert("Please fill in all required fields.");
+  }
+}
+
+getElement("#send").addEventListener("click", verifySubmission);
